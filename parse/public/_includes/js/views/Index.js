a496a595787caa6d6e35c1f4f5bd6d2b869f41f8
Jay Tang <jay.tang@analogfolk.com>
2015-06-15 16:33:54 +0100

    Setting up cloud code

--- parse/public/_includes/js/views/Index.js
+++ parse/public/_includes/js/views/Index.js
@@ -1,15 +1,1 @@
-// Welcome page view
-App.Views.Welcome = Backbone.View.extend({
-    el: ".content",
-    initialize: function() {
-        var self = this;
-        $.ajax("_includes/js/templates/welcome.tpl").success(function(html) {
-            self.template = _.template(html);
-            self.render();
-        });
-    },
-    render: function() {
-        this.$el.html(this.template());
-        return this;
-    }
-});
+App.Views.Welcome=Backbone.View.extend({el:".content",initialize:function(){var a=this;$.ajax("_includes/js/templates/welcome.tpl").success(function(b){a.template=_.template(b),a.render()})},render:function(){return this.$el.html(this.template()),this}});
